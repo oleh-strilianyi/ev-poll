c5f6cec440754552a8ae809a041675fc351f93ed
ostrilianyi <strelyanyi.oleg@gmail.com>
2026-02-07 17:24:27 +0200

    Make header stick to the top.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="relative z-10 bg-gradient-to-br from-indigo-950 via-purple-900 to-fuchsia-900 px-4 py-3 shadow-lg shadow-purple-900/20 rounded-b-xl border-b border-white/5 ring-1 ring-white/10">
+    <header className="sticky top-0 z-50 bg-gradient-to-br from-indigo-950 via-purple-900 to-fuchsia-900 px-4 py-3 shadow-lg shadow-purple-900/20 rounded-b-xl border-b border-white/5 ring-1 ring-white/10">
       <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-10 mix-blend-overlay pointer-events-none rounded-b-xl"></div>
       <div className="relative z-20 flex justify-start items-center">
         <div className="select-none">
